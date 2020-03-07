0141602f6f4c11917d1fb8ed9e25e2614c8fd814
rafamatoso <rafaelmatosog@gmail.com>
2020-03-07 20:41:02 -0300

    Project created

---
+++ server.js
@@ -0,0 +1,47 @@
+// server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const Pusher = require('pusher');
+
+const pusher = new Pusher({
+  //   appId: '',
+  //   key: '',
+  //   secret: '',
+  //   cluster: '',
+  //   encrypted: true
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
+  );
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+app.set('port', 5000);
+
+app.get('/', (req, res) => {
+  res.send('Welcome');
+});
+
+app.post('/prices/new', (req, res) => {
+  pusher.trigger('coin-prices', 'prices', {
+    prices: req.body.prices
+  });
+  res.sendStatus(200);
+});
+
+app.listen(app.get('port'), () => {
+  console.log('Node app is running on port', app.get('port'));
+});
